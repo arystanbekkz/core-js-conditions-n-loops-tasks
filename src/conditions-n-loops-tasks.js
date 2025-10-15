9bb50d35c2298ced41b7b7e2ac24833fe8e883b1
Arystanbek Baigeldinov <149990446+arystanbekkz@users.noreply.github.com>
2025-10-15 05:41:33 +0000

    feat: add shuffleChar

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -432,8 +432,27 @@ function sortByAsc(arr) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+function shuffleChar(str, iterations) {
+  let s = str;
+  const cache = {};
+  for (let i = 0; i < iterations; i += 1) {
+    let eS = '';
+    let oS = '';
+    if (cache[s]) {
+      s = cache[s];
+    } else {
+      for (let j = 0; j < str.length; j += 1) {
+        if (j % 2 === 0) {
+          eS += s[j];
+        } else {
+          oS += s[j];
+        }
+      }
+      cache[s] = eS + oS;
+      s = cache[s];
+    }
+  }
+  return s;
 }
 
 /**
